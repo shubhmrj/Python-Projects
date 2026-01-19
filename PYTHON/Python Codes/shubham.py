0cc66932af2925e16f5e233818e323db401473a5
Shubham Raj <shubmrj@gmail.com>
2026-01-19 05:52:45 +0530

    push

--- PYTHON/Python Codes/shubham.py
+++
@@ -1,4 +0,0 @@
-def hello():
-    print("hello shubham")
-if __name__== "__main__":
- hello()   
--- PYTHON/QUESTION/question and projects/append.py
+++
@@ -1,9 +0,0 @@
-sqr=[]
-x=int(input())
-
-for x in range(x):
-   
-    j=int(input())
-    
-    sqr.append(j)
-    print(sqr)
